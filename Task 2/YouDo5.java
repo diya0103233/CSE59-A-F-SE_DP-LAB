94417f2e2f81c4830d1eb7cd48a0a08b52345bd3
diya0103233 <diyasinha4378@gmail.com>
2026-06-09 11:07:04 +0600

    Update YouDo5.java

--- Task 2/YouDo5.java
+++ Task 2/YouDo5.java
@@ -1,1 +1,19 @@
 
+class printer{
+    
+    void printdata(String text){
+        System.out.println("text:"+ text);
+    }
+    void printdata(int number){
+        System.out.println("number:"+ number);
+    }
+}
+
+public class Main{
+    public static void main(String[] args){
+        
+        printer print = new printer();
+        print.printdata("hlw diya");
+        print.printdata(100);
+    }
+}
