439c7c65ed073cec9b9f3150096a44fd1449f590
diya0103233 <diyasinha4378@gmail.com>
2026-06-16 09:59:01 +0600

    Update YouDo.java

--- Task 3/YouDo.java
+++ Task 3/YouDo.java
@@ -1,1 +1,46 @@
+//user entity
+public class User {
+    private String username;
+    private String email;
+
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUsernAME() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
+
+//Email Validator
+public class EmailValidator {
+
+    public boolean isValid(String email) {
+        return email != null && email.contains("@")
+    }
+}
+
+//Repository Abstraction
+
+public interface UserRepository {
+    void save(User user);
+}
+
+//Database Implementation
+public class DatabaseUserRepository implements UserRepository {
+    @Override
+    public void save(save user) {
+        System.out.println("Connecting to database...");
+        System.out.println(
+            "Saving user " + user.getUsername() + " to the users table."
+        );
+    }
+}
+
 
