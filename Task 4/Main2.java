54c357f0f0a0b33ed1a4328362e537420b490677
diya0103233 <diyasinha4378@gmail.com>
2026-06-23 11:20:47 +0600

    Update Main2.java

--- Task 4/Main2.java
+++ Task 4/Main2.java
@@ -1,1 +1,45 @@
+class TeamMember {
+    protected String name;
+    
+    public TeamMember(String name) {
+        this.name = name;
+    }
+}
 
+interface BonusEligible {
+    void calculateBonus();
+}
+
+class Employee extends TeamMember implements BonusEligible {
+    
+    public Employee(String name) {
+        super(name);
+    }
+    
+    public void calculateBonus() {
+        System.out.println("Calculating standard employee bonus for " + name);
+        
+
+    }
+}
+
+class Contractor extends TeamMember {
+    
+    public Contractor(String name) {
+        super(name);
+    }
+    
+    public void work() {
+        System.out.println(name + "is working as a contractor. ");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Employee e = new Employee("Alice");
+        Contractor c = new Contractor("Bob");
+        
+        e.calculateBonus();
+        c.work();
+    }
+}
