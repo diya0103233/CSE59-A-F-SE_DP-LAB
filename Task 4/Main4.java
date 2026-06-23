a6bc43adbfe52420ae001ad0411a15b20afd894c
diya0103233 <diyasinha4378@gmail.com>
2026-06-23 11:29:25 +0600

    Update Main4.java

--- Task 4/Main4.java
+++ Task 4/Main4.java
@@ -1,1 +1,40 @@
+interface MessageSender {
+    void send(String message);
+}
 
+class EmailSender implements MessageSender {
+    public void send(String message) {
+        System.out.println("Sending email: " + message);
+    }
+}
+
+class SMSSender implements MessageSender {
+    public void send(String message) {
+        System.out.println("Sending SMS: " + message);
+    }
+}
+
+class NotificationService {
+    private MessageSender sender;
+
+    public NotificationService(MessageSender sender) {
+        this.sender = sender;
+    }
+
+    public void alertUser(String msg) {
+        sender.send(msg);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        MessageSender email = new EmailSender();
+        NotificationService service1 = new NotificationService(email);
+        service1.alertUser("Hello via Email");
+
+        MessageSender sms = new SMSSender();
+        NotificationService service2 = new NotificationService(sms);
+        service2.alertUser("Hello via SMS");
+    }
+}
