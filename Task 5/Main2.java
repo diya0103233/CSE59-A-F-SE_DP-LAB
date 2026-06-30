f72ed79bc5f25e747a5b473eca99a490f52d0873
diya0103233 <diyasinha4378@gmail.com>
2026-06-30 11:23:04 +0600

    Update Main2.java

--- Task 5/Main2.java
+++ Task 5/Main2.java
@@ -1,1 +1,23 @@
+interface MessageSender {
+    void sendMessage(String message);
+    
+}
 
+class EmailSender implements MessageSender {
+    @Override
+    public void sendMessage(String message) {
+        SAystem.out.println("Sending email: " + message):
+    }
+}
+
+class NotificationService {
+    private messageSender;
+    
+    public NotificationService(MessageSender messageSender) {
+        this.messageSender = messageSender;
+    }
+    
+    public void alertUser(String msg) {
+        messageSender.sendMessage(msg);
+    }
+}
