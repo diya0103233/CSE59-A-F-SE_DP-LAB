a3c9d2cbd5437ec107ec485f16d1153dabf830c7
diya0103233 <diyasinha4378@gmail.com>
2026-06-30 11:14:30 +0600

    Update Main7.java

--- Task 5/Main7.java
+++ Task 5/Main7.java
@@ -1,1 +1,22 @@
+interface INetworkProtocol {
+    void broadcastMessage(String msg);
+}
 
+class BluetoothMeshNetwork implements INetworkProtocol {
+    @Override
+    public void broadcastMessage(String msg) {
+        System.out.println("Broadcasting via Bluetooth Mesh: " + msg);
+    }
+}
+
+class DisasterAlertSystem {
+    private INetworkProtocol network;
+
+    public DisasterAlertSystem(INetworkProtocol network) {
+        this.network = network;
+    }
+
+    public void triggerAlert(String alertData) {
+        network.broadcastMessage(alertData);
+    }
+}
